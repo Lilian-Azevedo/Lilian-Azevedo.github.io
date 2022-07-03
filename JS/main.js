5eccbe89e2e979a72d5d87e90ced9153963d2407
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-07-03 14:17:46 -0300

    :sparkles: Feat:toggle skils content

--- JS/main.js
+++ JS/main.js
@@ -21,4 +21,21 @@ function linkAction(){
     navMenu.classList.add('hidden-menu');
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('.skills_header');
+
+skillsHeader.forEach(element => {
+    element.addEventListener('click', () => {
+        let itemClass = element.parentNode.className;
+        for (let index = 0; index < skillsContent.length; index++) {
+            const element = skillsContent[index];
+            element.className = 'skills_content skills_close';
+        }
+
+        if(itemClass === 'skills_content skills_close') {
+            element.parentNode.className = 'skills_content skills_open';
+        }
+    });
+}); 
