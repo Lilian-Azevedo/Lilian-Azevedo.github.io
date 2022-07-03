7eb798770e223d8196f8786a91e37f7582cf63a3
Lilian Azevedo <lilian.azevedo0905@gmail.com>
2022-07-03 20:42:20 -0300

    :sparkles: Feat:Qualification languages

--- JS/main.js
+++ JS/main.js
@@ -39,3 +39,23 @@ skillsHeader.forEach(element => {
         }
     });
 }); 
+
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+        tabContents.forEach(element => {
+            element.classList.remove('qualification_active');
+        });
+
+        target.classList.add('qualification_active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification_active');
+        })
+
+        tab.classList.add('qualification_active');
+    })
+})
